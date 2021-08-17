417695795e1b003517ba0e2b605541222f175e3f
circle33 <newnash@163.com>
2021-08-17 14:29:02 +0000

    feat: 适配微信 v3 分账接口

---
+++ src/Payment/ProfitsharingV3/Client.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\Payment\ProfitSharingV3;
+
+use EasyWeChat\Payment\Kernel\BaseClient;
+
+/**
+ * Class Client.
+ *
+ * @author circle33 <newnash13@gmail.com>
+ */
+class Client extends BaseClient
+{
+    /**
+     * {@inheritdoc}.
+     */
+    protected function prepends()
+    {
+        return [
+            'sign_type' => 'HMAC-SHA256',
+        ];
+    }
+
+    /**
+     * Multi profit sharing.
+     * 请求分账.
+     *
+     * @param string $transactionId 微信支付订单号
+     * @param string $outOrderNo    商户系统内部的分账单号
+     * @param array  $receivers     分账接收方列表
+     * @param bool   $finish        是否分账完成
+     *
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function share(
+        string $transactionId,
+        string $outOrderNo,
+        array $receivers,
+        bool  $finish = false,
+    ) {
+        $params = [
+            'appid' => $this->app['config']->app_id,
+            'transaction_id' => $transactionId,
+            'out_order_no' => $outOrderNo,
+            'receivers' => $receivers,
+            'finish' => $finish,
+        ];
+
+        return $this->safeRequest(
+            'v3/brand/profitsharing/orders',
+            $params
+        );
+    }
+}
---
+++ tests/Payment/ProfitSharingV3/ClientTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\Tests\Payment\ProfitSharingV3;
+
+use EasyWeChat\Payment\Application;
+use EasyWeChat\Payment\ProfitSharingV3\Client;
+use EasyWeChat\Tests\TestCase;
+
+class ClientTest extends TestCase
+{
+    protected function app()
+    {
+        return new Application([
+            'app_id' => 'wx123456',
+            'brand_mchid' => 'foo-brand-mchid',
+            'sub_mchid' => '1900000109',
+        ]);
+    }
+
+    public function testShare()
+    {
+        $client = $this->mockApiClient(
+            Client::class,
+            ['safeRequest'],
+            $this->app()
+        );
+
+        $client->expects()->safeRequest(
+            'v3/brand/profitsharing/orders',
+            [
+                'appid' => 'wx123456',
+                'transaction_id' => '4208450740201411110007820472',
+                'out_order_no' => 'P20150806125346',
+                'receivers' => [[
+                    'type' => 'MERCHANT_ID',
+                    'account' => '190001001',
+                    'amount' => 100,
+                    'description' => '分到商户',
+                ], [
+                    'type' => 'PERSONAL_WECHATID',
+                    'account' => '86693952',
+                    'amount' => 888,
+                    'description' => '分到个人',
+                ]],
+                "finish" => false,
+            ]
+        )->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->share(
+            '4208450740201411110007820472',
+            'P20150806125346',
+            [[
+                'type' => 'MERCHANT_ID',
+                'account' => '190001001',
+                'amount' => 100,
+                'description' => '分到商户',
+            ], [
+                'type' => 'PERSONAL_WECHATID',
+                'account' => '86693952',
+                'amount' => 888,
+                'description' => '分到个人',
+            ]]
+        ));
+    }
+}
